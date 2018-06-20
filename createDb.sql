2ee91ea6c9719a6e04046c1573822ac0eac180dd
dsari581 <40220644+dsari581@users.noreply.github.com>
2018-06-19 20:45:03 -0400

    added dorm and dorm rooms tables

--- createDb.sql
+++ createDb.sql
@@ -33,7 +33,35 @@ CREATE TABLE IF NOT EXISTS `its544db`.`faculty` (
 ENGINE = InnoDB
 AUTO_INCREMENT = 2
 DEFAULT CHARACTER SET = utf8;
+-- -----------------------------------------------------
+-- Deniz and Mustafa
+-- Table `its544db`.`dorm`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `its544db`.`dorm` (
+  `dormId` INT NOT NULL,
+  `dormName` VARCHAR(45) NULL,
+  `dormCapacity` VARCHAR(45) NULL,
+  PRIMARY KEY (`dormId`))
+ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `its544db`.`dorm rooms`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `its544db`.`dorm rooms` (
+  `roomNo` VARCHAR(45) NULL,
+  `roomCapacity` VARCHAR(45) NULL,
+  `dormId` INT NOT NULL,
+  INDEX `fk_dorm rooms_dorm_idx` (`dormId` ASC),
+  CONSTRAINT `fk_dorm rooms_dorm`
+    FOREIGN KEY (`dormId`)
+    REFERENCES `its544db`.`dorm` (`dormId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- ADD YOUR TABLES ABOVE THIS POINT
+               -
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
